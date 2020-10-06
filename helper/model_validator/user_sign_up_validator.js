9528c5fb7ff0cfe925fbbdfdc3f2a4e735716e79
apple <apple@apples-MacBook-Pro.local>
2020-10-07 02:03:50 +0530

    updated error messages for user validation

--- helper/model_validator/user_sign_up_validator.js
+++ helper/model_validator/user_sign_up_validator.js
@@ -2,23 +2,23 @@ const { body, validationResult } = require('express-validator')
 
 const userSignUpValidator = [
     body('name')
-        .not().isEmpty().withMessage('username should not be empty')
-        .isString().withMessage('username is not string')
-        .isLength({ min :3, max : 30}).withMessage('username length error')
+        .not().isEmpty().withMessage('Username should not be empty.')
+        .isString().withMessage('Username is not string.')
+        .isLength({ min :3, max : 30}).withMessage('Username length error.')
         .trim(),
     body('email')
-        .not().isEmpty().withMessage('email should not be empty')
-        .isEmail().withMessage("string is not email")
-        .isLength({ min : 3, max : 30}).withMessage('email length error')
+        .not().isEmpty().withMessage('Email should not be empty.')
+        .isEmail().withMessage("Please enter correcy email.")
+        .isLength({ min : 3, max : 30}).withMessage('Email length error.')
         .trim(),
     body('password')
-        .not().isEmpty().withMessage('password should not be empty')
-        .isLength({ min : 8, max : 30}).withMessage('password length error')
+        .not().isEmpty().withMessage('Password should not be empty.')
+        .isLength({ min : 8, max : 30}).withMessage('Password length error.')
         .trim(),
     body('mobileno')
-        .not().isEmpty().withMessage('mobileno should not be empty')
-        .isInt().withMessage('mobile number should be number')
-        .isLength({ min : 10, max : 30}).withMessage('mobile no length error')
+        .not().isEmpty().withMessage('Mobile No should not be empty.')
+        .isInt().withMessage('Mobile number incorrect.')
+        .isLength({ min : 10, max : 30}).withMessage('Mobile no length error.')
         .trim()
 ]
 
